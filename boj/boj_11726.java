1ad9bca08db19d5d9cf71e44092b188ed301357f
JerryK026 <kskyung0624@gmail.com>
2021-03-19 06:28:52 +0900

    11726 clear

--- boj/boj_11726.java
+++ boj/boj_11726.java
@@ -9,28 +9,15 @@ public class boj_11726 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        int cur = 0;
-        int count = 0;
-        int tmp;
+        int cache[] = new int[n + 3];
 
-        if(n % 2 == 0) count++;
-
-        while(n > 0) {
-            tmp = 1;
-            // 가로와 세로로 나눌 수 있으므로, 같은 것이 있는 순열로 해결할 수 있다. (가로 + 세로)! / (가로! * 세로!)
-            for(int i = n + 1; i <= n + cur; i++) {
-                tmp *= i;
-                for(int j = 2; j <= cur; j++) {
-                    tmp /= j;
-                }
-                tmp %= 10007;
-            }
-            count += tmp;
-            n -= 2;
-            cur += 1;
+        cache[1] = 1;
+        cache[2] = 2;
+        for(int i = 3; i <= n; i++) {
+            cache[i] = cache[i - 1] + cache[i - 2] % 10007;
         }
 
-        bw.write(String.valueOf(count % 10007));
+        bw.write(String.valueOf(cache[n] % 10007));
         bw.flush();
         bw.close();
     }
--- boj/boj_1463.java
+++ boj/boj_1463.java
@@ -3,9 +3,6 @@ package boj;
 import java.io.*;
 
 public class boj_1463 {
-    static int N;
-    static int count = 0;
-
     // 나눗셈은 몫, 나머지, 3보다 작은 수 때문에 복잡 -> 1에서부터 곱하는 식으로 가면 나눗셈이랑 같다는 데서 착안
     // 어차피 count의 수만 세면 됨
     // 33 같은 경우 33 -> 11 -> 10 -> 9 -> 3 -> 1 이렇게 되므로 해당 방법대론 안됨
@@ -14,46 +11,20 @@ public class boj_1463 {
     // 1091 -> 1009 -> 509 -> ... 이렇게 되므로 13이 됨.
     // 어떤 기준으로 3을 나누고 2를 나눠야 할지 모르겠음 ㅇㅅㅇ
 
-    public static void d3() {
-        N = N / 3;
-        count++;
-    }
-    public static void d2() {
-        N = N / 2;
-        count++;
-    }
-    public static void sub() {
-        N -= 1;
-        count++;
-    }
-    public static void dec() {
-        if ((N - 1) % 3 == 0) {
-            sub();
-            d3();
-        }
-        else {
-            if (N % 2 != 0) {
-                sub();
-            }
-            d2();
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int[] cache = new int[N + 1];
 
-         while (N > 3) {
-             if (N % 3 == 0) {
-                 d3();
-             }
-             else dec();
+        for(int i = 2; i <= N; i++) {
+            if(i % 3 == 0) cache[i] = cache[i / 3] + 1;
+            else if(i % 2 == 0) cache[i] = (cache[i - 1] < cache[i / 2])? cache[i - 1] + 1 : cache[i / 2] + 1;
+            else cache[i] = cache[i - 1] + 1;
         }
-         if (N > 1) count++;
 
-        bw.write(String.valueOf(count));
+        bw.write(String.valueOf(cache[N]));
         bw.flush();
         bw.close();
     }
