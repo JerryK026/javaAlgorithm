e1b23393ebe338387d2701860c15e791f21acd1c
JerryK <kskyung0624@gmail.com>
2021-04-27 10:11:04 +0900

    10989 CountingSort pass

---
+++ boj/boj_10989.java
@@ -0,0 +1,23 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_10989 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[10001];
+        for(int i = 0; i < N; i++) {
+            arr[Integer.parseInt(br.readLine())]++;
+        }
+        for(int i = 1; i < 10001; i++) {
+            for(int j = 0; j < (arr[i]); j++) {
+                sb.append(i + "\n");
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
