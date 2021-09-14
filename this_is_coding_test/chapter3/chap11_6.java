3a80c8d8f57fd3d46c2af2cfa3a02c3470b848de
SeokyungKim <kskyung0624@gmail.com>
2021-09-14 23:14:58 +0900

    this code. chap 11-6. greedy

---
+++ this_is_coding_test/chapter3/chap11_6.java
@@ -0,0 +1,58 @@
+package this_is_coding_test.chapter3;
+
+import java.util.*;
+
+public class chap11_6 {
+    static class Food {
+        int time;
+        int idx;
+        public Food(int time, int idx) {
+            this.time = time;
+            this.idx = idx;
+        }
+    }
+
+    Comparator<Food> timeComp = new Comparator<>() {
+        public int compare(Food a, Food b) {
+            return a.time - b.time;
+        }
+    };
+
+    Comparator<Food> idxComp = new Comparator<>() {
+        public int compare(Food a, Food b) {
+            return a.idx - b.idx;
+        }
+    };
+
+    public int solution(int[] food_times, long k) {
+        List<Food> lst = new LinkedList<Food>();
+        int n = food_times.length;
+
+        for (int i = 0; i < n; i++) {
+            lst.add(new Food(food_times[i], i + 1));
+        }
+
+        lst.sort(timeComp);
+
+        int cache = 0;
+        int i = 0;
+        for (Food f: lst) {
+            long diff = f.time - cache;
+            if (f.time != 0) {
+                long spend = diff * n;
+                if (spend > k) {
+                    k %= n;
+                    lst.subList(i, food_times.length).sort(idxComp);
+                    return lst.get(i + (int)k).idx;
+                } else {
+                    k -= spend;
+                    cache = f.time;
+                }
+            }
+            i++;
+            n--;
+        }
+
+        return -1;
+    }
+}
