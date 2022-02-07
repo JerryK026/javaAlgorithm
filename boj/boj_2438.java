d38ce6c88434b38a8914ec558d0c7d76469b5f80
SeokyungKim <kskyung0624@gmail.com>
2022-02-07 11:36:49 +0900

    별찍기-1 수학으로 해결

---
+++ boj/boj_2438.java
@@ -0,0 +1,22 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_2438 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j <= i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
