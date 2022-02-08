ec24faaff809f49652de9cfb0a027c65b3f79b06
SeokyungKim <kskyung0624@gmail.com>
2022-02-08 15:15:27 +0900

    N과 M(3). 재귀로 해결

---
+++ boj/boj_15651.java
@@ -0,0 +1,40 @@
+package boj;
+
+import java.io.*;
+
+public class boj_15651 {
+    static final StringBuilder sb = new StringBuilder();
+    static int N;
+    static int M;
+    static int[] answer;
+
+    static void dfs(int depth) {
+        if (depth == M) {
+            for (int val : answer) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            answer[depth] = i + 1;
+            dfs(depth + 1);
+        }
+        return;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final String[] input = br.readLine().split(" ");
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+
+        answer = new int[M];
+
+        dfs(0);
+
+        System.out.println(sb.toString());
+    }
+}
